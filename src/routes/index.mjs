a7e41784ef8cb0a302565c0a9f4f202b4a203a60
Juvazquez <juvazquez616@gmail.com>
2025-09-03 00:58:44 -0300

    Se agrego rutas para el profile

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -1,10 +1,11 @@
 import express from "express";
 import paisRouter from "./paisRouter.mjs";
 import authRouter from "./authRouter.mjs";
-
+import profileRouter from "./profileRouter.mjs";
 const router = express.Router();
 
 router.use("/paises", paisRouter);
 router.use("/auth", authRouter);
+router.use("/profiles", profileRouter);
 
 export default router;
